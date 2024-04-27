4b6b7dcb020ec71963e4e46eb956403e7483d8fd
loonwy <157209226+loonwy@users.noreply.github.com>
2024-04-27 06:45:48 +0300

    binary search

--- lesson6/massiv.py
+++ lesson6/massiv.py
@@ -1,20 +1,21 @@
-def find_division_index(arr):
-    left, right = 0, len(arr) - 1
+def find_division_index(a):
+    left, right = 0, len(a) - 1
     while left <= right:
-        midpoint = left + (right - left) // 2
-        if arr[midpoint] == 1 and arr[midpoint - 1] == 0:
-            return midpoint
-        elif arr[midpoint] == 0:
-            left = midpoint + 1
+        center = left + (right - left) // 2
+        if a[center] == 1 and a[center - 1] == 0:
+            return center
+        elif a[center] == 0:
+            left = center + 1
         else:
-            right = midpoint - 1
+            right = center - 1
     return -1
 
-arr = list(map(int, input("Введите упорядоченный массив из 0 и 1 через пробел: ").split()))
-result = find_division_index(arr)
+a = list(map(int, input("Введите массив из 0 и 1: ").split()))
+a.sort()
+result = find_division_index(a)
+print(a)
 
 if result != -1:
-    print(f"Место разделения: Индекс {result}")
+    print(f"Index {result}")
 else:
-    print("Разделение не найдено.")
-print (arr)
+    print("Разделение не обнаружено")
